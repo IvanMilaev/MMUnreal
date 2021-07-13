ee29c43d794072689fda8f2ba06c802dd6db2638
milaevivan <milaevivan@protonmail.com>
2021-07-13 14:47:47 +0200

    initial

---
+++ Source/MMUnreal/MMUnrealGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MMUnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MMUNREAL_API AMMUnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
